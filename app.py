98499c0c21fde50582b924bb58bc4fc11f789076
Yanchick <yan@yanchick.org>
2016-06-30 15:45:59 +0600

    append tornado :)
    Signed-off-by: Yanchick <yan@yanchick.org>

---
+++ app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8-*-
+#!/usr/bin/env python
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+import string  
+import json 
+import time
+import tornado.ioloop
+import tornado.httpserver
+import tornado.web
+import tornado.websocket
+import tornado.log as t_l
+import tornado.options
+import threading
+from tornado.log import enable_pretty_logging
+APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(APP_ROOT, '..'))
+
+  
+class TornadoApplication(tornado.web.Application):
+    
+    def __init__(self):
+        path  =  os.path.abspath(os.path.join(os.path.dirname(__file__)))+"/static/"
+        handlers = [
+             (r"/(.*)", tornado.web.StaticFileHandler, {"path": path, "default_filename": "index.htm"})
+,
+        ]
+        settings = {
+            "debug": True,
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+           
+            
+http_server = tornado.httpserver.HTTPServer(TornadoApplication())
+http_server.listen(8080)
+tornado.ioloop.IOLoop.instance().start()        
+
--- static/app.js
+++ static/app.js
@@ -2,6 +2,8 @@ var app = angular.module('app', ['ngTouch', 'ui.grid','leaflet-directive']);
 
 app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {	
 
+	 
+	
   $scope.go = function() {
 	  $http({
 		  method: 'GET',
@@ -17,28 +19,39 @@ app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
 		    // or server returns response with an error status.
 		  });
 
-	    
-	    
-	    $scope.myData = [
-	                     {
-	                         "firstName": "Cox",
-	                         "lastName": "Carney",
-	                         "company": "Enormo",
-	                         "employed": true
-	                     },
-	                     {
-	                         "firstName": "Lorммсвысаraine",
-	                         "lastName": "Wise",
-	                         "company": "Comveyer",
-	                         "employed": false
-	                     },
-	                     {
-	                         "firstName": "Nancy",
-	                         "lastName": "Waters",
-	                         "company": "Fuelton",
-	                         "employed": false
-	                     }
-	                 ];
+	  
+	  $http({
+		  method: 'POST',
+		  url: 'http://10.184.5.68:1887/zhat/GetEmplDepart'
+		}).then(function successCallback(response) {
+			a =  angular.fromJson(response);
+			 $scope.myData  =  a.data.RefEmplDepart;
+		    // this callback will be called asynchronously
+		    // when the response is available
+		  }, function errorCallback(response) {
+			  $scope.name = 'clicked';
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+	  
+	  angular.extend($scope, {
+	        cairo: {
+	            lat: 30.05,
+	            lng: 31.25,
+	            zoom: 10
+	        },
+	        tiles: {
+	            url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+	        },
+	        defaults: {
+	            scrollWheelZoom: false
+	        }
+	    });
+	   $scope.$watch("cairo.zoom", function(zoom) {
+	        $scope.tiles.url = (zoom > 12) ? "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+	                : "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
+	    });
+	  
 	  };
   
 }]);
