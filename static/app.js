9dfe9fa0493f1bbc8082a0d7a7cdc93417fd54a2
Yanchick <yan@yanchick.org>
2016-07-04 09:57:32 +0600

    Add drone badge
    Signed-off-by: Yanchick <yan@yanchick.org>

--- static/app.js
+++ static/app.js
@@ -1,10 +1,7 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid','leaflet-directive']);
+var app = angular.module('app', ['ui.grid','leaflet-directive', 'ui.grid.edit', 'ui.grid.cellNav']);
 
 app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {	
-
-	 
-	
-  $scope.go = function() {
+  $scope.BtnClick = function() {
 	  $http({
 		  method: 'GET',
 		  url: 'http://10.184.5.68:1887/zhat/get_utc'
@@ -33,25 +30,6 @@ app.controller('MainCtrl', ['$scope','$http', function ($scope,$http) {
 		    // called asynchronously if an error occurs
 		    // or server returns response with an error status.
 		  });
-	  
-	  angular.extend($scope, {
-	        cairo: {
-	            lat: 30.05,
-	            lng: 31.25,
-	            zoom: 10
-	        },
-	        tiles: {
-	            url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-	        },
-	        defaults: {
-	            scrollWheelZoom: false
-	        }
-	    });
-	   $scope.$watch("cairo.zoom", function(zoom) {
-	        $scope.tiles.url = (zoom > 12) ? "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-	                : "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
-	    });
-	  
 	  };
   
 }]);
